1ad304f7f75b4f7abb262f29181d7f279597c514
carlosmontes002 <carlosmontes002@gmail.com>
2016-06-15 22:33:22 -0400

    change endpoints /audio & /video now using ServletOutputStream

--- src/cams/streaming/v_a/Audio.java
+++ src/cams/streaming/v_a/Audio.java
@@ -1,15 +1,11 @@
 package cams.streaming.v_a;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/audio")
 public class Audio extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final long EXPIRE_TIME = 1000 * 60 * 60 * 24;	
+	private static final long serialVersionUID = 1L;	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,49 +29,21 @@ public class Audio extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String audioFilename = URLDecoder.decode("audio.mp3", "UTF-8");
-	    String audioPath = "c:\\test\\";
-		Path audio = Paths.get(audioPath, audioFilename);
-		
-		int length = (int) Files.size(audio);
-	    int start = 0;
-	    int end = length - 1;
-	 
-	    String range = request.getHeader("Range");	    	   
-	    
-	    String rangeValues = range.substring(range.indexOf("=") + 1, range.length());
-	    String rangeSplit [] = rangeValues.split("-");
-	    
-	    start = rangeSplit[0].isEmpty() ? start : Integer.valueOf(rangeSplit[0]);
-	    start = start < 0 ? 0 : start;
-	    
-	    end = rangeSplit.length == 1 ? end : Integer.valueOf(rangeSplit[1]);
-	    end = end > length - 1 ? length - 1 : end;   
-	 
-	    System.out.println("range: " + range);
-	    System.out.println("start: " + start);
-	    System.out.println("end: " + end);
-	 
-	    response.reset();
-	    response.setBufferSize(1024);
-	    response.setDateHeader("Last-Modified", Files.getLastModifiedTime(audio).toMillis());
-	    response.setHeader("Content-Disposition", String.format("inline;filename=\"%s\"", audioFilename));
-	    response.setHeader("Accept-Ranges", "bytes"); 	
-	    response.setHeader("Content-Range", String.format("bytes %s-%s/%s", start, end, length));
-	    response.setDateHeader("Expires", System.currentTimeMillis() + EXPIRE_TIME);
-	    response.setContentType(Files.probeContentType(audio));
-	    response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-	 
-	    BufferedInputStream input = new BufferedInputStream(new FileInputStream (audio.toString()), 1024);
-	    BufferedOutputStream output = new BufferedOutputStream(response.getOutputStream(), 1024);
-	    
-	    byte [] buffer = new byte[1024];
-	    int count;	    
-	    while ((count = input.read(buffer)) > 0) {
-	    	output.write(buffer, 0, count);
-        }
-	    input.close();
-	    output.close();
+		ServletOutputStream out = response.getOutputStream();
+		InputStream in = new FileInputStream ("c:/test/audio.mp3");
+		String mimeType = "audio/mp3";
+		byte[] bytes = new byte[1024];
+		int bytesRead;
+
+		response.setContentType(mimeType);
+
+		while ((bytesRead = in.read(bytes)) != -1) {
+		    out.write(bytes, 0, bytesRead);
+		}
+
+		// do the following in a finally block:
+		in.close();
+		out.close();
 	}
 
 	/**
